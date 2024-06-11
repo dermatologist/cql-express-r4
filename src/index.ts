51fa22cf9f33a2018334e5dd6c6eabc5bdec6744
Bell Eapen <github_public@gulfdoctor.net>
2024-06-11 16:52:23 +0000

    chore: Update index.ts to parse cqlJson, assign it to elmFile, and add cql-exec-vsac module and import statements

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import cql from "cql-execution";
 import cqlfhir from "cql-exec-fhir";
 import cqlvsac from "cql-exec-vsac";
+import fs from "fs";
+import path from "path";
 /*
  * Load up and parse configuration details from
  * the `.env` file to the `process.env`
@@ -27,12 +29,41 @@ app.get("/", (req: Request, res: Response) => {
 
 app.post("/exec", (req: Request, res: Response) => {
     const umlsKey = process.env.UMLS_API_KEY || "";
-    const fhirBundle = req.body.fhirBundle;
+    const fhirBundles = req.body.fhirBundles;
     const cqlJson = req.body.cqlJson;
+    const fhirBaseUrl = req.body.fhirBaseUrl || "";
     const elmFile = JSON.parse(cqlJson);
+    const libraries = {
+      FHIRHelpers: JSON.parse(fs.readFileSync("FHIRHelpers.json", "utf8")),
+    };
+    const library = new cql.Library(elmFile, new cql.Repository(libraries));
 
+    // Create the patient source
+    let patientSource = cqlfhir.PatientSource.FHIRv401();
+    patientSource.loadBundles(fhirBundles);
+      // Extract the value sets from the ELM
+  let valueSets = [];
+  if (elmFile.library && elmFile.library.valueSets && elmFile.library.valueSets.def) {
+    valueSets = elmFile.library.valueSets.def;
+  }
+
+  // Set up the code service, loading from the cache if it exists
+  const codeService = new cqlvsac.CodeService(path.join(__dirname, 'vsac_cache'), true);
+  // Ensure value sets, downloading any missing value sets
+  codeService.ensureValueSetsWithAPIKey(valueSets, umlsKey, fhirBaseUrl)
+    .then(() => {
+
+      // Value sets are loaded, so execute!
+      const executor = new cql.Executor(library, codeService);
+      executor.exec(patientSource).then((results) => {
+        res.send(results);
+      }).catch((err) => {
+        console.log(err);
+      }
+      );
+    });
+  res.send("Error");
 });
-
 /* Start the Express app and listen
  for incoming requests on the specified port */
 app.listen(port, () => {
