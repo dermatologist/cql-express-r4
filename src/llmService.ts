2e0c9c39b0a1b88f88e0c90e890e717f497e1df5
Bell Eapen <github_public@gulfdoctor.net>
2024-06-16 15:22:33 +0000

    Refactor llmService.ts for improved code structure and error handling

--- src/llmService.ts
+++ src/llmService.ts
@@ -2,11 +2,43 @@ import { BaseChain } from "medpromptjs";
 
 
 class LlmService extends BaseChain {
+  string_expression: string = "";
 
   async checkAssertion(expression, context): Promise<boolean> {
-    console.log("\nChecking assertion with expression: ", expression);
-    console.log("\n and context: ", context);
+    const _expression = JSON.parse(expression);
+    const _context = JSON.parse(context);
+    let _content = ""
+    console.log("\nChecking assertion with expression: ", this.printValues(_expression));
+    _context.forEach(element => {
+          _content += atob(element.content[0].attachment.data.value);
+    });
+    console.log("\n and context: ", _content);
     return true;
+  }
+
+  printValues(obj) {
+    for (var key in obj) {
+        if (typeof obj[key] === "object") {
+            this.printValues(obj[key]);
+        } else {
+
+              this.string_expression += obj[key] + " ";
+
+        }
+    }
+    const _eliminate = ["true", "false", "null", "undefined", "String",
+      "Number", "Object", "Array", "Boolean", "value", "Type", "Specifier", "Named"]
+    _eliminate.forEach(element => {
+      this.string_expression = this.string_expression.replace(element, '');
+    });
+    this.string_expression = this.string_expression.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '')
+    return this.camelToString(this.string_expression);
+  }
+
+
+  camelToString(camelCase) {
+    return camelCase.replace(/([A-Z])/g, ' $1')
+        .replace(/^./, function(str){ return str.toUpperCase(); });
   }
 
   async checkMention(expression, context): Promise<boolean> {
