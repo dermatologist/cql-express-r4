426ca64bfa6023073c82f346946d94166db5d1c5
dermatologist <github@gulfdoctor.net>
2025-05-24 21:16:13 -0500

    Update dependencies and refactor bootstrap logic for improved clarity and maintainability

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,31 +1,25 @@
 import "reflect-metadata";
 import { container } from "tsyringe";
-import { GoogleVertexAI } from "langchain/llms/googlevertexai";
-import {Ollama } from "@langchain/community/llms/ollama";
-import { ChatPromptTemplate} from "langchain/prompts";
+import { Ollama } from "@langchain/community/llms/ollama";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { pull } from "langchain/hub";
 
 import { z } from "zod";
 import { DynamicTool, DynamicStructuredTool } from "@langchain/core/tools";
 
-
 const bootstrap = async () => {
-    const google_vertex_ai = new GoogleVertexAI({
-        model: "text-bison@001",
-        temperature: 0.2,
-    });
 
-    const ollama = new Ollama({
-        baseUrl: "http://localhost:11434",
-        model: "phi3"
-    });
+  const ollama = new Ollama({
+    baseUrl: "http://localhost:11434",
+    model: "phi3",
+  });
 
-    // const prompt = await pull<ChatPromptTemplate>(
-    //     "hwchase17/structured-chat-agent"
-    // );
+  // const prompt = await pull<ChatPromptTemplate>(
+  //     "hwchase17/structured-chat-agent"
+  // );
 
-    const prompt = ChatPromptTemplate.fromTemplate(
-        `
+  const prompt = ChatPromptTemplate.fromTemplate(
+    `
         Knowing:
         A year has 365 days and a month has 30 days.
         If it is less than a given period, then it is within the period.
@@ -37,40 +31,37 @@ const bootstrap = async () => {
         {expression}
 
         Answer should begin with true or false. Then give a reason why.`
-    );
-    const tools = [
-        new DynamicTool({
-            name: "FOO",
-            description:
-            "call this to get the value of foo. input should be an empty string.",
-            func: async () => "baz",
-        }),
-        new DynamicStructuredTool({
-            name: "random-number-generator",
-            description: "generates a random number between two input numbers",
-            schema: z.object({
-            low: z.number().describe("The lower bound of the generated number"),
-            high: z.number().describe("The upper bound of the generated number"),
-            }),
-            func: async ({ low, high }) =>
-            (Math.random() * (high - low) + low).toString(), // Outputs still must be strings
-        }),
-    ];
+  );
+  const tools = [
+    new DynamicTool({
+      name: "FOO",
+      description:
+        "call this to get the value of foo. input should be an empty string.",
+      func: async () => "baz",
+    }),
+    new DynamicStructuredTool({
+      name: "random-number-generator",
+      description: "generates a random number between two input numbers",
+      schema: z.object({
+        low: z.number().describe("The lower bound of the generated number"),
+        high: z.number().describe("The upper bound of the generated number"),
+      }),
+      func: async ({ low, high }) =>
+        (Math.random() * (high - low) + low).toString(), // Outputs still must be strings
+    }),
+  ];
 
+  container.register("main-llm", {
+    useValue: ollama,
+  });
+  container.register("tools", {
+    useValue: tools,
+  });
+  container.register("prompt", {
+    useValue: prompt,
+  });
 
-    container.register("main-llm", {
-        useValue: google_vertex_ai,
-    });
-    container.register("tools", {
-        useValue: tools,
-    });
-    container.register("prompt", {
-        useValue: prompt,
-    });
-
-
-
-    return container;
-}
+  return container;
+};
 
 export default bootstrap;
